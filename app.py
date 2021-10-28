946cb9a6cb5711778ddff9b2e5218a64700b1d54
MattB859 <mateus756@hotmail.com>
2021-10-28 12:47:06 +0000

    Create and add new review section for the_secret.html which allows users to view their current posts

--- app.py
+++ app.py
@@ -30,12 +30,18 @@ def book_reviews():
     return render_template("book_power.html", reviews=reviews)
 
 
+@app.route("/the_secret_book")
+def the_secret_book():
+    reviews = mongo.db.reviews_2.find()
+    return render_template("the_secret.html", reviews=reviews)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         # check if username already exists in the database
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username": request.form.get("username").capitalize()})
         existing_email = mongo.db.users.find_one(
             {"email_address": request.form.get("email_address")})
 
@@ -47,14 +53,14 @@ def register():
             return redirect(url_for("register"))
 
         register = {
-            "username": request.form.get("username").lower(),
+            "username": request.form.get("username").capitalize(),
             "email_address": request.form.get("email_address"),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
+        session["user"] = request.form.get("username").capitalize()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
@@ -65,13 +71,13 @@ def login():
     if request.method == "POST":
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            {"username": request.form.get("username").capitalize()})
 
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
+                    session["user"] = request.form.get("username").capitalize()
                     flash("Welcome, {}".format(
                         request.form.get("username")))
                     return redirect(url_for(
@@ -95,7 +101,7 @@ def profile(username):
     return render_template("profile.html", username=username)
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template("profile.html", username=username).capitalize()
 
         return redirect(url_for("login"))
 
@@ -126,7 +132,23 @@ def book_power():
         return redirect(url_for("book_power"))
         
         reviews = list(mongo.db.reviews.find())
-    return render_template("book_power.html")    
+    return render_template("book_power.html")
+
+
+@app.route("/the_secret", methods=["GET", "POST"])
+def the_secret():
+    if request.method == "POST":
+        the_secret_review = {
+            "headline": request.form.get("headline"),
+            "the_secret_review": request.form.get("the_secret_review"),
+            "created_by": session["user"]
+        }
+        mongo.db.reviews_2.insert_one(the_secret_review)
+        flash("Review Successfully Added")
+        return redirect(url_for("the_secret_book"))
+        
+        reviews_2 = list(mongo.db.reviews_2.find())
+    return render_template("the_secret.html")
 
 
 if __name__ == "__main__":
