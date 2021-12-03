445d090836ef9f41b3d83cc604496c215fcb38aa
MattB859 <mateus756@hotmail.com>
2021-12-03 10:49:11 +0000

    DEBUG mode turned off

--- app.py
+++ app.py
@@ -443,4 +443,4 @@ def delete_user(user_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
